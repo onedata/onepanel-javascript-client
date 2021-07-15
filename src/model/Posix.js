6c39cbb524438a71b298a1e0b8b9b90ed038e655
Bamboo Agent <bamboo@onedata.org>
2021-07-15 12:15:46 +0000

    Auto update: VFS-8016 add rootUid, rootGid properties to posix storage

--- src/model/Posix.js
+++ src/model/Posix.js
@@ -57,6 +57,8 @@
     _this['type'] = type;
     _this['mountPoint'] = mountPoint;
 
+
+
   };
 
   /**
@@ -90,6 +92,12 @@
       if (data.hasOwnProperty('storagePathType')) {
         obj['storagePathType'] = ApiClient.convertToType(data['storagePathType'], 'String');
       }
+      if (data.hasOwnProperty('rootUid')) {
+        obj['rootUid'] = ApiClient.convertToType(data['rootUid'], 'Number');
+      }
+      if (data.hasOwnProperty('rootGid')) {
+        obj['rootGid'] = ApiClient.convertToType(data['rootGid'], 'Number');
+      }
     }
     return obj;
   }
@@ -113,6 +121,16 @@
    * @default 'canonical'
    */
   exports.prototype['storagePathType'] = 'canonical';
+  /**
+   * Admin uid on posix storage.
+   * @member {Number} rootUid
+   */
+  exports.prototype['rootUid'] = undefined;
+  /**
+   * Admin gid on posix storage.
+   * @member {Number} rootGid
+   */
+  exports.prototype['rootGid'] = undefined;
 
   // Implement StorageCreateDetails interface:
   /**

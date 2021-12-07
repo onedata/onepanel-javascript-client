ce2abe0d261ee5994307e4f041da0162eed448d6
Bamboo Agent <bamboo@onedata.org>
2021-12-07 14:17:33 +0000

    Auto update: VFS-8419 block flat paths in posix and webdav storages

--- src/model/Webdav.js
+++ src/model/Webdav.js
@@ -179,8 +179,8 @@
    */
   exports.prototype['dirMode'] = '0775';
   /**
-   * Determines how the logical file paths will be mapped on the storage. 'canonical' paths reflect the logical file names and directory structure, however each rename operation will require renaming the files on the storage. 'flat' paths are based on unique file UUID's and do not require on-storage rename when logical file name is changed. 
-   * @member {String} storagePathType
+   * Determines how the logical file paths will be mapped on the storage. 'canonical' paths reflect the logical file names and directory structure, however each rename operation will require renaming the files on the storage. 'flat' paths are based on unique file UUID's and do not require on-storage rename when logical file name is changed. Note, that 'flat' paths are not allowed on this type of storage. 
+   * @member {module:model/Webdav.StoragePathTypeEnum} storagePathType
    * @default 'canonical'
    */
   exports.prototype['storagePathType'] = 'canonical';
@@ -316,6 +316,18 @@ exports.prototype['onedataAccessToken'] = undefined;
      * @const
      */
     "sabredav": "sabredav"  };
+
+  /**
+   * Allowed values for the <code>storagePathType</code> property.
+   * @enum {String}
+   * @readonly
+   */
+  exports.StoragePathTypeEnum = {
+    /**
+     * value: "canonical"
+     * @const
+     */
+    "canonical": "canonical"  };
 
 
   return exports;

b7b9bd05ab8cc09f9872d8ef78e0e6950ba0b010
Bamboo Agent <bamboo@onedata.org>
2025-09-19 12:21:08 +0000

    Auto update: Pull request #285: VFS-12608 Corrected OneS3 params decription

--- src/model/ServiceOnes3.js
+++ src/model/ServiceOnes3.js
@@ -91,7 +91,7 @@
    */
   exports.prototype['hosts'] = undefined;
   /**
-   * The port on which the OneS3 service will be available.  NOTE: This parameter is taken into account only during the initial cluster  deployment and ignored when adding new hosts to the cluster. 
+   * The port on which the OneS3 service will be available.  NOTE: This parameter is considered only during the initial cluster deployment or when adding the OneS3 service for the first time. In all other cases, it is ignored, and the port will default to that of the already deployed OneS3 services in the cluster. 
    * @member {Number} port
    */
   exports.prototype['port'] = undefined;

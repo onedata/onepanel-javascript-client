e674220301112df91cb2791a74ea1298b7e4c27e
Bamboo Agent <bamboo@onedata.org>
2026-05-12 13:56:16 +0000

    Auto update: VFS-13447 Experimental GuiMessage.externalUrl

--- docs/GuiMessage.md
+++ docs/GuiMessage.md
@@ -5,5 +5,6 @@ Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **enabled** | **Boolean** | True if the message should be displayed. | [optional] 
 **body** | **String** | HTML content of the message to display. | [optional] 
+**externalUrl** | **String** | If specified, GUI will link to this page instead of presenting the message body itself. The body will be ignored in this case. | [optional] 
 
 
--- src/model/GuiMessage.js
+++ src/model/GuiMessage.js
@@ -51,6 +51,7 @@
 
 
 
+
   };
 
   /**
@@ -80,6 +81,9 @@
       if (data.hasOwnProperty('body')) {
         obj['body'] = ApiClient.convertToType(data['body'], 'String');
       }
+      if (data.hasOwnProperty('externalUrl')) {
+        obj['externalUrl'] = ApiClient.convertToType(data['externalUrl'], 'String');
+      }
     }
     return obj;
   }
@@ -94,6 +98,11 @@
    * @member {String} body
    */
   exports.prototype['body'] = undefined;
+  /**
+   * If specified, GUI will link to this page instead of presenting the message body itself. The body will be ignored in this case.
+   * @member {String} externalUrl
+   */
+  exports.prototype['externalUrl'] = undefined;
 
 
 
